5db5d4a9b3ef020ce67308c6126de6ce469637a0
anthonydurso <42847034+anthonydurso@users.noreply.github.com>
2018-12-03 11:05:11 -0500

    Update RobotShop.py
    Modified

--- RobotShop.py
+++ RobotShop.py
@@ -1,37 +1,59 @@
-productNames = [ "Ultrasonic range finder"
- , "Servo motor"
- , "Servo controller"
- , "Microcontroller Board"
- , "Laser range finder"
- , "Lithium polymer battery"
- ]
-productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
-productQuantities = [ 4, 10, 5, 7, 2, 8 ]
+
+
+class Product:
+    def __int__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock
+
+
+products = [Product("Ultrasonic Range Finder", 2.50, 4),
+            Product("Servo Motor", 14.99, 10),
+            Product("Servo Controller", 44.95, 5),
+            Product("Microcontroller", 34.95, 7),
+            Product("Laser Range Finder", 149.99, 2),
+            Product("Lithium Polymer Battery", 8.99, 8)]
+            
+
+
 def printStock():
- print()
- print("Available Products")
- print("------------------")
- for i in range(0,len(productNames)):
- if productQuantities[i] > 0:
- print(str(i)+")",productNames[i], "$", productPrices[i])
- print()
+    print()
+    
+    print("Available Products")
+    
+    print("------------------")
+    
+    for i in range(0,len(products.name)):
+        if products.stock[i] > 0:
+            print(str(i)+")",products.name[i], "$", products.price[i])
+            print()
+
+
+ 
 def main():
- cash = float(input("How much money do you have? $"))
- while cash > 0:
- printStock()
- vals = input("Enter product ID and quantity you wish to buy: ").split(" ")
- if vals[0] == "quit": break
- prodId = int(vals[0])
- count = int(vals[1])
- if productQuantities[prodId] >= count:
- if cash >= productPrices[prodId] * count:
- productQuantities[prodId] -= count
- cash -= productPrices[prodId] * count
- print("You purchased", count, productNames[prodId]+".")
- print("You have $", "{0:.2f}".format(cash), "remaining.")
- else:
- print("Sorry, you cannot afford that product.")
- else:
- print("Sorry, we are sold out of", productNames[prodId])
+    cash = float(input("How much money do you have? $"))
+    while cash > 0:
+        printStock()
+        
+        vals = input("Enter product ID and quantity you wish to buy: ").split(" ")
+
+        if vals[0] == "quit": break
+        
+        prodId = int(vals[0])
+        
+        count = int(vals[1])
+        
+    if products.stock[prodId] >= count:
+         if cash >= products.price[prodId] * count:
+             products.stock[prodId] -= count
+             cash -= products.price[prodId] * count
+             print("You purchased", count, products.name[prodId]+".")
+             print("You have $", "{0:2f}".format(cash), "remaining.")
+         else:
+             print("Sorry, you cannot afford that product.")
+
+    else:
+        print("Sorry, we are sold out of", products.name[prodId])
+
 main()
 
